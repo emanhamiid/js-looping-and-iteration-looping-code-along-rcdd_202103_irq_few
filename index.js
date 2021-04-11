6fccda56eb3dbed0d546060206136c18da0cd571
eman hamid <emanhharki@gmail.com>
2021-04-11 19:55:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+let arr = [];
+ for (let i = 0; i < names.length; i++) {
+   arr[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+ }
+ return arr;
+}
+
+
+function countDown(n){
+  let i = 10;
+  while(i >=0){
+    console.log(i);
+    i--;
+  }
+}
+countDown(10);
